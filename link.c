9b3005d7558a435525a909af8e3f08317fbd7057
piyushsharma6715 <piyushsharma6715@gmail.com>
2016-11-11 15:58:29 +0530

    Added full program

--- link.c
+++ link.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node insert_start(struct node *);
+struct node* insert_start(struct node *);
+struct node* insert_end(struct node *);
+struct node* insert_middle(struct node *);
+struct node* delete_first(struct node *);
+void delete_end(struct node *);
 void display(struct node *);
 struct node
 {
@@ -15,7 +19,7 @@ void main()
 	start->next=NULL;
 	do
 	{
-		printf("ENTER \n 1. FOR INSERTION  \n 2. FOR DISPLAY\n 3. FOR EXIT\n");
+		printf("\n ENTER \n 1. FOR INSERTION  \n 2. FOR DELETION\n 3. FOR DISPLAY\n 4. FOR EXIT\n");
 		scanf("%d",&ch);
 		if(ch==1)
 		{
@@ -27,16 +31,34 @@ void main()
 			}
 			else
 			{
-				*start=insert_start(start);
+				printf("\n ENTER \n1. FOR INSERTION AT START\n 2. FOR INSERTION AT END\n 3. FOR INSERTION AT MIDDLE");
+				scanf(" %d",&ch1);
+				if(ch1==1)
+				start=insert_start(start);
+				else if(ch1==2)
+				start=insert_end(start);
+				else if(ch1==3)
+				start=insert_middle(start);
+				else
+				printf("YOU HAVE ENTERED A WRONG VALUE FOR INSERTION");
 			}
 		}
 		else if(ch==2)
 		{
+			printf("\nENTER \n1.DELETION AT START\n2.DELETION AT END");
+			scanf("\n%d",&ch1);
+			if(ch1==1)
+				start=delete_first(start);
+			else if(ch1==2)
+				delete_end(start);
+		}
+		else if(ch==3)
+		{
 			display(start);
 		}
-	}while(ch!=3);
+	}while(ch!=4);
 }
-struct node insert_start(struct node *start)
+struct node* insert_start(struct node *start)
 {
 	struct node *temp;
 	temp=(struct node*)malloc(sizeof(struct node));
@@ -44,10 +66,98 @@ struct node insert_start(struct node *start)
 	printf("ENTER THE NUMBER");
 	scanf(" %d",&temp->data);
 	temp->next=start ;
-printf("temp->next %d start %d",temp->next,start);
 	start=temp;
-	printf("temp adress %d start.data %d  start.next %d strat add %d  temp .next %d   ",temp,start->data,start->next,start,temp->next);
-	return *start;
+	return start;
+}
+struct node* insert_end(struct node *start)
+{
+	struct node *temp,*temp1;
+	temp=(struct node*)malloc(sizeof(struct node));
+	temp1=(struct node*)malloc(sizeof(struct node));
+	temp->next=NULL;
+	printf("ENTER THE NUMBER ");
+	scanf("%d",&temp->data);
+	temp1=start;
+	while(1)
+	{
+	if(temp1->next!=NULL)
+	temp1=temp1->next;
+	else
+	break;
+	}
+	temp1->next=temp;
+	return start;
+}
+struct node* insert_middle(struct node *start)
+{
+	struct node *temp,*temp1;
+	int d,e,f=0;
+	temp=(struct node *)malloc(sizeof(struct node));
+	temp1=(struct node *)malloc(sizeof(struct node));
+	temp->next=NULL;
+	temp1->next=NULL;
+	printf("ENTER THE NUMBER");
+	scanf("%d",&temp->data);
+	printf("ENTER THE POSITION OF INSERTION");
+	scanf("%d",&d);
+	temp1=start;
+	e=d;
+	while(1)
+	{
+	     if(e==1)
+		  break;
+	     else
+	     {
+		while(f!=e-2)
+		{
+			temp1=temp1->next;
+			f++;
+		}
+		e=1;
+	     }
+	}
+	if(d==1)
+	{
+		temp->next=temp1;
+		start=temp;
+	}
+	else
+	{
+		temp->next=temp1->next;
+		temp1->next=temp;
+	}
+	return start;
+}
+struct node* delete_first(struct node *start)
+{
+struct node *temp;
+temp=(struct node*)malloc(sizeof(struct node));
+temp=start;
+start=start->next;
+free(temp);
+return start;
+}
+void delete_end(struct node *start)
+{
+	int n=-1;
+	struct node *temp,*temp1;
+	temp=(struct node*)malloc(sizeof(struct node));
+	temp1=(struct node*)malloc(sizeof(struct node));
+	temp=start;
+	while(temp!=NULL)
+	{
+		temp=temp->next;
+		n++e;
+	}
+	temp=start;
+	while((n-1)!=0)
+	{
+		temp=temp->next;
+		n--;
+	}
+	temp1=temp->next;
+	temp->next=NULL;
+	free(temp1);
 }
 void display(struct node *start)
 {
@@ -57,13 +167,13 @@ void display(struct node *start)
 	}
 	else if(start->next==NULL)
 	{
-		printf(" %d ",start->data);
+		printf("%d",start->data);
 	}
 	else
 	{
 	      while(start!=NULL)
 		{
-			printf("%d----->",start->data);
+			printf(" %d ----->",start->data);
 			start=start->next;
 		}
 	}
